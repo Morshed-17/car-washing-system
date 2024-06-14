782deaed084ddb1ab917ee0727ae0098694bf903
Kazi Golam Morshed <120186837+Morshed-17@users.noreply.github.com>
2024-06-14 22:26:12 +0600

    Delete src/app/modules/booking/booking.route2.ts

--- src/app/modules/booking/booking.route2.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import auth from '../../middlewares/auth';
-import { BookingControllers } from '../booking/booking.controller';
-
-const router = express.Router();
-
-router.get('/', auth('user'), BookingControllers.getUserBooking);
-export const BookingRoutes2 = router;
